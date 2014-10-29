a78fb3b22cba2e85a2c0b89992e981a262aef6c9
Michael W Stevens <micste@us11mac00039.local>
2014-10-29 13:41:41 -0400

    added a test class

---
+++ MWSTest.h
@@ -0,0 +1,13 @@
+//
+//  MWSTest.h
+//  Git Test
+//
+//  Created by Michael W Stevens on 10/29/14.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MWSTest : NSObject
+
+@end
---
+++ MWSTest.m
@@ -0,0 +1,13 @@
+//
+//  MWSTest.m
+//  Git Test
+//
+//  Created by Michael W Stevens on 10/29/14.
+//  Copyright (c) 2014 Code Coalition. All rights reserved.
+//
+
+#import "MWSTest.h"
+
+@implementation MWSTest
+
+@end
